b7e557bac72431576f7fbf14dc377761a10783c9
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-02-13 09:01:07 +0530

    feat

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ let unreads=document.querySelectorAll(".unread");
 let markRead=document.querySelector("#read");
 
 for (let item of unreads){
-    item.addEventListener('click', function myFunc(event) {
-        removeBullet(item)
-        item.classList.remove('unread');
+    item.addEventListener('click', function myFunc() {
+        makeRead(item)
         item.removeEventListener("click", myFunc);
     })
 }
 
-function removeBullet(item){
+markRead.addEventListener('click', function () {
+    console.log('clicked');
+    unreads.forEach(makeRead); //handle for individuals after clicking on some
+});
+
+function makeRead(item){
     let text=item.querySelector('.activity-text').querySelector('.activities');
     let bullet=text.querySelector('.bullet');
     bullet.remove();
+    item.classList.remove('unread');
 }
